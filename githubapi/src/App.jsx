085a10c709f58d5a5213bcb8018d60a9135f41bc
bbiomax <bbbiomax@gmail.com>
2024-08-02 16:11:39 +0300

    search layout

---
+++ githubapi/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Search from './components/search/Search';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='content'>
+        <Search />
+      </div>
+    </div>
+  );
+}
+
+export default App;
